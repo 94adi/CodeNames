6dad141df86c078efb51b9db758f470106a59aa1
Adrian Cotuna <adrianrcotuna@gmail.com>
2025-01-20 11:00:11 +0200

    End guess button + various improvements (#2)

--- CodeNames.Core/Services/GameRoomService/GameRoomService.cs
+++ CodeNames.Core/Services/GameRoomService/GameRoomService.cs
@@ -1,44 +1,55 @@
-﻿using CodeNames.Models;
-using CodeNames.Repository;
+﻿
+namespace CodeNames.CodeNames.Core.Services.GameRoomService;
 
-namespace CodeNames.CodeNames.Core.Services.GameRoomService
+public class GameRoomService : IGameRoomService
 {
-    public class GameRoomService : IGameRoomService
+    private readonly IGameRoomRepository _gameRoomRepository;
+
+    public GameRoomService(IGameRoomRepository gameRoomRepository)
     {
-        private readonly IGameRoomRepository _gameRoomRepository;
+        _gameRoomRepository = gameRoomRepository;
+    }
 
-        public GameRoomService(IGameRoomRepository gameRoomRepository)
+    public IEnumerable<GameRoom> GetGameRooms()
+    {
+        return _gameRoomRepository.GetAll();
+    }
+
+    public GameRoom GetRoomByName(string name) =>
+                _gameRoomRepository.GetRoomByName(name);
+
+    public void InvalidateInvitationCode(int gameRoomId, Guid code)
+    {
+        var gameRoom = _gameRoomRepository.Get(gameRoomId);
+        if(gameRoom == null)
         {
-            _gameRoomRepository = gameRoomRepository;
+            throw new Exception("Could not find Game Room");
         }
 
-        public IEnumerable<GameRoom> GetGameRooms()
+        if(gameRoom.InvitationCode == code)
         {
-            return _gameRoomRepository.GetAll();
+            gameRoom.InvitationCode = Guid.NewGuid();
+            _gameRoomRepository.Update(gameRoom);
         }
+    }
 
-        public GameRoom GetRoomByName(string name) =>
-                    _gameRoomRepository.GetRoomByName(name);
+    public bool IsGameRoomValid(string gameRoom)
+    {
+        var result = _gameRoomRepository.GetRoomByName(gameRoom) != null ? true : false;
 
+        return result;
+    }
 
-        public bool IsGameRoomValid(string gameRoom)
-        {
-            var result = _gameRoomRepository.GetRoomByName(gameRoom) != null ? true : false;
+    public bool IsInvitationCodeValid(string gameRoom, Guid code)
+    {
+        var gameRoomResult  = _gameRoomRepository.GetRoomByName(gameRoom);
 
-            return result;
-        }
+        if (gameRoomResult == null) 
+            throw new Exception("Game Room could not be found!");
 
-        public bool IsInvitationCodeValid(string gameRoom, Guid code)
-        {
-            var gameRoomResult  = _gameRoomRepository.GetRoomByName(gameRoom);
+        if (gameRoomResult.InvitationCode == code)
+            return true;
 
-            if (gameRoomResult == null) 
-                throw new Exception("Game Room could not be found!");
-
-            if (gameRoomResult.InvitationCode == code)
-                return true;
-
-            return false;
-        }
+        return false;
     }
 }
